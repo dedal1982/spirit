aee10f9447a398a965a8a3cc652185fee5a7b66a
aldm <d44.a1@yandex.ru>
2024-01-13 02:40:03 +0300

    wd

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const burgerClick = document.querySelector(".header__burger");
 const scrollLock = document.querySelector(".page");
+const mobMenu = document.querySelector(".mobile-menu");
+
 //открытие бургера
 burgerClick.addEventListener("click", () => {
   burgerClick.classList.toggle("active");
-
+  mobMenu.classList.toggle("active");
   scrollLock.classList.toggle("lock");
 });
 
@@ -12,7 +14,7 @@ window.addEventListener("scroll", function () {
   var header = document.getElementById("sticky-header");
   var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollPosition > 900) {
+  if (scrollPosition > 400) {
     header.style =
       "backdrop-filter: blur(8px) ;background:linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),";
     header.style.background =
