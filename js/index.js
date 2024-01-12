43218b3077bef19466ba96401619790948d3095f
aldm <d44.a1@yandex.ru>
2024-01-13 00:30:15 +0300

    цв

--- js/index.js
+++ js/index.js
@@ -13,7 +13,10 @@ window.addEventListener("scroll", function () {
   var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollPosition > 900) {
-    header.style = "backdrop-filter: blur(8px);";
+    header.style =
+      "backdrop-filter: blur(8px) ;background:linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),";
+    header.style.background =
+      "linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))";
   } else {
     header.style = "backdrop-filter: none;";
   }
