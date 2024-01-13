bc3150f7b23594d4aae8d95a7f0e2bee87370e32
aldm <d44.a1@yandex.ru>
2024-01-13 15:14:08 +0300

    wda

---
+++ js/swipper.js
@@ -0,0 +1,16 @@
+const newtSlider = new Swiper(".swipp__swiper", {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
