a93ffd07e74a9145f2f1b6f61ef3f304a3f91ad2
aldm <d44.a1@yandex.ru>
2024-01-16 18:48:27 +0300

    dw

--- js/swipper.js
+++ js/swipper.js
@@ -19,3 +19,27 @@ const swiper = new Swiper(".catalog__swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiperShop = new Swiper(".cheat-shop-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 40,
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
